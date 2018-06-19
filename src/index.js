18d4a29e145965982e3da8bb3fd7ef268b133a01
wdd119 <1240172019@qq.com>
2018-06-19 22:48:09 +0800

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './styles/iconfont/iconfont.css'
-import App from './component/app';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
